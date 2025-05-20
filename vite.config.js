3ef02a707358dea861c792a5f29b4f8bf516ca2d
peter <wstcpyt@gmail.com>
2025-05-20 12:01:48 -0700

    change allowed host

--- vite.config.js
+++ vite.config.js
@@ -24,7 +24,7 @@ export default defineConfig({
   server: {
     host: '0.0.0.0',
     port: 3000,
-    allowedHosts: 'all',
+    allowedHosts: true,   // respond to *any* Host header
     watch: {
       usePolling: true,
       interval: 300,   // ms; tune if CPU gets high
